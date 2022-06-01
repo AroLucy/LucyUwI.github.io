2e84068a3052f24130996cad221aec647cffd09e
LucyUwI <lucyclelland06@gmail.com>
2022-06-01 11:04:56 +0100

    Added diary page

---
+++ daily/apply.js
@@ -0,0 +1,22 @@
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                document.getElementById('output').textContent=allText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+function show() {
+    readTextFile("./daily.txt")
+    document.getElementById("hideAfter").classList.toggle("hide")
+}
