6770869abc4a69e6ef8bb593a76666583f825209
Lucy <73023435+LucyUwI@users.noreply.github.com>
2022-07-30 23:25:11 +0100

    color() and theme() recombined

--- media/app.js
+++ media/app.js
@@ -2,7 +2,9 @@ var toggled = localStorage.getItem('ThemeToggled')
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
     const newColorScheme = event.matches ? "dark" : "light";
-    color();
+    document.body.classList.toggle("darkmode")
+    document.getElementById("button1").classList.toggle("DarkIcon")
+    document.getElementById("button2").classList.toggle("DarkIcon")
 });
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -11,14 +13,10 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
     document.getElementById("button2").classList.toggle("DarkIcon")
 }
 
-function color() {
+function theme() {
     document.body.classList.toggle("darkmode")
     document.getElementById("button1").classList.toggle("DarkIcon")
     document.getElementById("button2").classList.toggle("DarkIcon")
-}
-
-function theme() {
-    color()
     var toggled = localStorage.getItem('ThemeToggled')
     if (toggled != 'true') {
         localStorage.setItem('ThemeToggled', 'true')
