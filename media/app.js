dc9318bce89fcc53f0e8a6798c1e0c56964f5071
Lucy <73023435+LucyUwI@users.noreply.github.com>
2022-09-02 16:20:12 +0100

    Some Clean up and Fixed some issues with /Projects

--- media/app.js
+++ media/app.js
@@ -1,20 +1,26 @@
 var toggled = localStorage.getItem('ThemeToggled')
 
+if (document.getElementById("ProjectsHTML") !== null) {
+    body = document.getElementById("ProjectsHTML");
+} else {
+    body = document.body
+}
+
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
     const newColorScheme = event.matches ? "dark" : "light";
-    document.body.classList.toggle("darkmode")
+    body.classList.toggle("darkmode")
     document.getElementById("button1").classList.toggle("DarkIcon")
     document.getElementById("button2").classList.toggle("DarkIcon")
 });
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.body.classList.toggle("darkmode")
+    body.classList.toggle("darkmode")
     document.getElementById("button1").classList.toggle("DarkIcon")
     document.getElementById("button2").classList.toggle("DarkIcon")
 }
 
 function theme() {
-    document.body.classList.toggle("darkmode")
+    body.classList.toggle("darkmode")
     document.getElementById("button1").classList.toggle("DarkIcon")
     document.getElementById("button2").classList.toggle("DarkIcon")
     var toggled = localStorage.getItem('ThemeToggled')
@@ -26,7 +32,7 @@ function theme() {
 }
 
 if (toggled == 'true') {
-    document.body.classList.toggle("darkmode")
+    document.getElementsByTagName("html").toggle("darkmode")
     document.getElementById("button1").classList.toggle("DarkIcon")
     document.getElementById("button2").classList.toggle("DarkIcon")
 }
